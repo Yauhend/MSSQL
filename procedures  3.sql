453b18c9c3b7ff5fa9a6ef951f8a492abebd6857
Yauhend <30414218+Yauhend@users.noreply.github.com>
2017-12-19 23:47:11 +0300

    news

---
+++ procedures  3.sql
@@ -0,0 +1,29 @@
+﻿----Создать хранимую процедуру: добавления студента на указанный факультет и форму обучения 
+----(входные данные название факультета и наименование формы обучения, дата рождения дата поступления  )
+
+create proc student_add @v_faculty nvarchar(10), @v_form nvarchar (15), @v_birthdate date, @v_indate date
+as 
+	declare @iv_fac int, @iv_form int
+	select @iv_fac =  faculty.id from faculty where faculty_name=@v_faculty
+	select @iv_form = form.id from form where form.form_name=@v_form
+	if exists(select * from hours	where hours.faculty_id=@iv_fac and hours.form_id=@iv_form and hours.course = 1)
+		begin
+			begin tran
+				insert into stud(last_name, f_name, br_date, in_date)
+				values ('!Blank', '!Blank', @v_birthdate, @v_indate)
+				insert into process(stud_id,hours_id)
+				values ((select top 1 id from stud order by id desc), (select top 1 id from hours	where hours.faculty_id=@iv_fac and hours.form_id=@iv_form and hours.course = 1))
+			commit
+			select 'New student added' as message
+			return
+		end
+	else
+		begin
+			select 'Can not find 1st course on this faculty and form' as error
+			return
+		end
+go
+
+
+student_add 'ФПК','заочно', '1983-12-12', '2016-12-12' 
+
---
+++ procedures 2 and 3.sql
@@ -0,0 +1,28 @@
+﻿--Создать хранимую процедуру:
+--Подсчета количества студентов на факультете и форме обучения (входные данные название факультета и наименование формы обучения )
+
+
+
+create proc student_count @v_faculty nvarchar(10), @v_form nvarchar (15)
+as 
+	declare @iv_fac int, @iv_form int, @iv_sum int
+	select @iv_fac =  faculty.id from faculty where faculty_name=@v_faculty
+	select @iv_form = form.id from form where form.form_name=@v_form
+	if exists(select * from hours	where hours.faculty_id=@iv_fac and hours.form_id=@iv_form)
+		begin
+			select @iv_sum = count(*) 
+			from stud 
+				join process on process.stud_id = stud.id
+				join hours on hours.id = process.hours_id
+			where hours.faculty_id = @iv_fac and hours.form_id = @iv_form
+			select @iv_sum as Number_of_Students
+			return
+		end
+	else
+		begin
+			select 'Can not find faculty or form' as error
+			return
+		end
+go
+
+student_count 'ФПК','заочно'
